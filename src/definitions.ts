9208ea828716b21258478f90a71cea8be885681c
cpandya25 <110810122+cpandya25@users.noreply.github.com>
2023-11-17 18:50:57 +0530

    hotfix/CP-7021-capacitor-sdk-fix-the-return-values-of-the-promises (#15)
    * hotfix/CP-7021-capacitor-sdk-fix-the-return-values-of-the-promises
    
    fix the return values of the promises.
    
    * hotfix/CP-7021-capacitor-sdk-fix-the-return-values-of-the-promises
    
    fix the return values of the promises.

--- src/definitions.ts
+++ src/definitions.ts
@@ -4,27 +4,27 @@ import type { PluginListenerHandle } from '@capacitor/core';
 export interface CleverPushCapacitorPlugin {
   init(options: { channelId: string, autoRegister: boolean }): void;
   subscribe(): Promise<{ subscriptionId: string }>;
-  unsubscribe(): Promise<{ unSubscribe: boolean }>;
+  unsubscribe(): Promise<{ success: boolean }>;
   isSubscribed(): Promise<{ isSubscribed: boolean }>;
   getSubscriptionId(): Promise<{ subscriptionId: string }>;
   showTopicsDialog(): void;
   enableDevelopmentMode(): void;
-  trackPageView(options: { url: string }): Promise<void>;
-  trackEvent(options: { eventName: string, properties?: Record<string, unknown> }): Promise<void>;
-  addSubscriptionTag(options: { tagId: string }): Promise<void>;
-  removeSubscriptionTag(options: { tagId: string }): Promise<void>;
+  trackPageView(options: { url: string }): Promise<{ success: boolean }>;
+  trackEvent(options: { eventName: string, properties?: Record<string, unknown> }): Promise<{ success: boolean }>;
+  addSubscriptionTag(options: { tagId: string }): Promise<{ success: boolean }>;
+  removeSubscriptionTag(options: { tagId: string }): Promise<{ success: boolean }>;
   hasSubscriptionTag(options: { tagId: string}): Promise<{ hasTag: boolean }>;
   getSubscriptionTags(): Promise<{ tagIds: string[] }>;
   getSubscriptionTopics(): Promise<{ topicIds: string[] }>;
-  setSubscriptionTopics(options: { topics: string[] }): Promise<void>;
+  setSubscriptionTopics(options: { topics: string[] }): Promise<{ success: boolean }>;
   getAvailableTopics(): Promise<{ topics: { _id: string; name: string; }[] }>;
-  setAuthorizerToken(options: { token: string }): Promise<void>;
+  setAuthorizerToken(options: { token: string }): Promise<{ success: boolean }>;
   getNotifications(): Promise<{ notifications: any[] }>;
-  setSubscriptionAttribute(options: { attributeId: string; value: string }): Promise<void>;
+  setSubscriptionAttribute(options: { attributeId: string; value: string }): Promise<{ success: boolean }>;
   getSubscriptionAttribute(options: { attributeId: string}): Promise<{ value: string }>;
   getSubscriptionAttributes(): Promise<{ attributes: { attributeId: string; value: string; }[] }>;
   getAvailableAttributes(): Promise<{ attributes: { attributeId: string; value: string; }[] }>;
-  setShowNotificationsInForeground(options: { showNotifications: boolean }): Promise<void>;
+  setShowNotificationsInForeground(options: { showNotifications: boolean }): Promise<{ success: boolean }>;
 
   addListener(
     eventName: 'notificationReceived',
