4deae501980e56ea3c9650fc95c5e660599f99df
cpandya25 <c.pandya@cleverpush.com>
2024-08-28 12:06:53 +0530

    hotfix/CP-8402-capacitor-sdk-exceptions-iOS
    Resolved issue of CleverPush" plugin is not implemented on ios updating capacitor from 5 to 6.

--- ios/Plugin/CleverPushCapacitorPlugin.h
+++ ios/Plugin/CleverPushCapacitorPlugin.h
@@ -1,6 +1,4 @@
 #import <UIKit/UIKit.h>
-#import <Capacitor/CAPPlugin.h>
-#import <Capacitor/CAPBridgedPlugin.h>
 
 //! Project version number for Plugin.
 FOUNDATION_EXPORT double PluginVersionNumber;
@@ -8,17 +6,3 @@ FOUNDATION_EXPORT double PluginVersionNumber;
 //! Project version string for Plugin.
 FOUNDATION_EXPORT const unsigned char PluginVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Plugin/PublicHeader.h>
-
-
-@class CAPPluginCall;
-
-@interface CleverPushCapacitorPlugin : CAPPlugin <CAPBridgedPlugin>
-
-@property (nonatomic, strong) CAPPluginCall *pluginCallDelegate;
-@property (class, nonatomic, strong) NSDictionary *pendingLaunchOptions;
-@property (nonatomic, strong) NSString *identifier;
-@property (nonatomic, strong) NSString *jsName;
-@property (nonatomic, strong) NSArray *pluginMethods;
-
-@end
