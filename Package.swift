ea31db4bd472671fd3cd6cf332ecc76cc05e8aad
cpandya25 <c.pandya.ext@cleverpush.com>
2026-08-24 16:30:40 +0530

    feature/cp-12376-cleverpush-capacitor-sdk-incompatibility-with-swift-package
    Added feature of Swift Package Manager support for iOS.

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CleverpushCapacitorSdk",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "CleverpushCapacitorSdk",
+            targets: ["CleverpushCapacitorSdk"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
+        .package(url: "https://github.com/cleverpush/cleverpush-ios-sdk-spm.git", from: "1.34.52")
+    ],
+    targets: [
+        .target(
+            name: "CleverpushCapacitorSdk",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "CleverPush", package: "cleverpush-ios-sdk-spm")
+            ],
+            path: "ios/Sources/CleverpushCapacitorSdk",
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath(".")
+            ]
+        )
+    ]
+)
